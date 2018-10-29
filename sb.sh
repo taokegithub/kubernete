51df1a1937396ff46c218ae5b43dfb31165868c1
tky <1529940294@qq.com>
2018-10-29 14:05:00 +0800

    delet sb

--- sb.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-images=(kube-controller-manager:v1.12.2 kube-apiserver:v1.12.2 kube-scheduler:v1.12.2 kube-proxy:v1.12.2 pause:3.1 etcd:3.2.24 coredns:1.2.2)
-for image in ${images[@]}
-do
-	docker pull shayu/$image
-	docker tag shayu/$image gcr.io/google_containers/$image
-	docker rmi shayu/$image
-done
